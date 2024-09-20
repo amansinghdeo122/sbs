9212fcc6b61e34c9fdb0a9498cb3c6bbf42db02d
aman <amankumardeo2016@gmail.com>
2024-09-20 14:50:12 +0530

    animation banner

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,7 +6,7 @@ const scroll = new LocomotiveScroll({
 var swiper = new Swiper('.swiper-container', {
   loop: true, // Loop through slides
   autoplay:true,
-  autoplayTimeout:2000,
+  autoplayTimeout: 6000,
   autoplayHoverPause:true,
   pagination: {
       el: '.swiper-pagination',
@@ -22,7 +22,7 @@ var swiper = new Swiper('.swiper-container', {
 var swiper = new Swiper('.testimal-tow', {
   loop: true,
   autoplay: {
-    delay: 2000,
+    delay: 3000,
     pauseOnMouseEnter: true // This replaces autoplayHoverPause
   },
   pagination: {
@@ -30,3 +30,26 @@ var swiper = new Swiper('.testimal-tow', {
     clickable: true,
   }
 });
+
+var tl = gsap.timeline();
+
+tl.from(".header, .header-inn, .logo", {
+  y: -30,
+  opacity: 0,
+  delay: 0.5,
+  duration: 1,
+  ease: "power2.out"
+});
+tl.from(".slider-bann-inn .left-sec", {
+  x: -100,
+  opacity: 0,
+  duration: 1.5,
+  ease: "power2.out",
+} ,"bann");
+
+tl.from(".slider-bann-inn .right-sec", {
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+  ease: "power2.out",
+} ,"bann");
